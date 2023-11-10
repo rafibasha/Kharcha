495d484fa8810d58b44d8035f861c9e832cfb166
rafibasha <rafibasha.sk@gmail.com>
2023-11-10 18:32:59 +0530

    Firebase integrated

--- App.js
+++ App.js
@@ -15,8 +15,9 @@ import {
   Text,
   useColorScheme,
   View,
+  Button
 } from 'react-native';
-
+import analytics from '@react-native-firebase/analytics';
 
 
 
@@ -27,6 +28,17 @@ function App() {
       <Text>Hello</Text>
       <Text>Hello</Text>
       <Text style={{backgroundColor:'yellow'}}>Hello2</Text>
+      <Button
+        title="Press me"
+        // Logs in the firebase analytics console as "select_content" event
+        // only accepts the two object properties which accept strings.
+        onPress={async () =>
+          await analytics().logSelectContent({
+            content_type: 'clothing',
+            item_id: 'abcd',
+          })
+        }
+      />
     </View>
   );
 }
